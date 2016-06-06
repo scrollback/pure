34f3bda3d32fc72c14f078f0932f12f5f4e2c4b1
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-06-06 15:16:21 +0530

    refactor(ui): move DiscussionActionSheet to separate component

--- src/ui/components/views/Discussion/DiscussionItem.js
+++ src/ui/components/views/Discussion/DiscussionItem.js
@@ -10,18 +10,12 @@ import DiscussionAuthor from './DiscussionAuthor';
 import DiscussionActions from './DiscussionActions';
 import TouchFeedback from '../Core/TouchFeedback';
 import Icon from '../Core/Icon';
-import ActionSheet from '../Core/ActionSheet';
-import ActionSheetItem from '../Core/ActionSheetItem';
+import DiscussionActionSheet from './DiscussionActionSheet';
 import Colors from '../../../Colors';
-import { convertRouteToURL } from '../../../../lib/Route';
-import { config } from '../../../../core-client';
 import { TAG_POST_HIDDEN } from '../../../../lib/Constants';
 import type { Thread, ThreadRel } from '../../../../lib/schemaTypes';
 
 const {
-	Clipboard,
-	Linking,
-	ToastAndroid,
 	StyleSheet,
 	TouchableOpacity,
 	PixelRatio,
@@ -103,52 +97,6 @@ export default class DiscussionItem extends Component<void, Props, State> {
 		return shallowCompare(this, nextProps, nextState);
 	}
 
-	_copyToClipboard: Function = text => {
-		Clipboard.setString(text);
-		ToastAndroid.show('Copied to clipboard', ToastAndroid.SHORT);
-	};
-
-	_handleOpenImage: Function = () => {
-		const { thread } = this.props;
-
-		if (thread.meta) {
-			const { photo } = thread.meta;
-
-			Linking.openURL(photo.url);
-		}
-	};
-
-	_handleCopyImageLink: Function = () => {
-		const { thread } = this.props;
-
-		if (thread.meta) {
-			const { photo } = thread.meta;
-
-			this._copyToClipboard(photo.url);
-		}
-	};
-
-	_handleCopyTitle: Function = () => {
-		this._copyToClipboard(this.props.thread.name);
-	};
-
-	_handleCopySummary: Function = () => {
-		this._copyToClipboard(this.props.thread.body);
-	};
-
-	_handleCopyLink: Function = () => {
-		const { thread } = this.props;
-
-		this._copyToClipboard(config.server.protocol + '//' + config.server.host + convertRouteToURL({
-			name: 'chat',
-			props: {
-				room: thread.parents[0],
-				thread: thread.id,
-				title: thread.name,
-			},
-		}));
-	};
-
 	_handleShowMenu: Function = () => {
 		this.setState({
 			actionSheetVisible: true,
@@ -223,38 +171,17 @@ export default class DiscussionItem extends Component<void, Props, State> {
 					</View>
 				</TouchFeedback>
 
-				<ActionSheet visible={this.state.actionSheetVisible} onRequestClose={this._handleRequestClose}>
-					<ActionSheetItem onPress={this._handleCopyTitle}>
-						Copy title
-					</ActionSheetItem>
-
-					{thread.meta && thread.meta.photo ? [
-						<ActionSheetItem key='open-image' onPress={this._handleOpenImage}>
-							Open image in browser
-						</ActionSheetItem>,
-						<ActionSheetItem key='copy-imagelink' onPress={this._handleCopyImageLink}>
-							Copy image link
-						</ActionSheetItem>,
-					] :
-						<ActionSheetItem onPress={this._handleCopySummary}>
-							Copy summary
-						</ActionSheetItem>
-					}
-
-					<ActionSheetItem onPress={this._handleCopyLink}>
-						Copy discussion link
-					</ActionSheetItem>
-
-					{isUserAdmin ?
-						hidden ?
-							<ActionSheetItem onPress={this.props.unhideThread}>
-								Unhide discussion
-							</ActionSheetItem> :
-							<ActionSheetItem onPress={this.props.hideThread}>
-								Hide discussion
-							</ActionSheetItem> : null
-					}
-				</ActionSheet>
+				<DiscussionActionSheet
+					thread={thread}
+					threadrel={threadrel}
+					isUserAdmin={isUserAdmin}
+					hideThread={this.props.hideThread}
+					unhideThread={this.props.unhideThread}
+					banUser={this.props.banUser}
+					unbanUser={this.props.unbanUser}
+					visible={this.state.actionSheetVisible}
+					onRequestClose={this._handleRequestClose}
+				/>
 			</Card>
 		);
 	}
