4aeca019200098817af236a180bf9c8c5d81cf7d
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-07-27 19:37:34 +0530

    fix(ui): fix signout

--- src/ui/middlewares/signoutMiddleware.native.js
+++ src/ui/middlewares/signoutMiddleware.native.js
@@ -3,19 +3,15 @@
 import { LoginManager } from 'react-native-fbsdk';
 import GoogleSignIn from '../../ui/modules/GoogleSignIn';
 import { bus } from '../../core-client';
+import store from '../../modules/store/store';
+import { resetSession } from '../../modules/store/actions';
 import type { Action } from '../../modules/store/SimpleStoreTypes';
 
 export default function(action: Action) {
 	switch (action.type) {
 	case 'SIGNOUT':
 		bus.emit('signout'); // FIXME: temporary
-		bus.emit('change', {
-			state: {
-				session: null,
-				user: null,
-				initialURL: null,
-			},
-		});
+		store.dispatch(resetSession());
 		LoginManager.logOut();
 		GoogleSignIn.signOut();
 		break;
